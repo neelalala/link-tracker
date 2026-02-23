0b3db09f9268f4953c516c1898218f2acba8637b
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-02-23 15:38:35 +0700

    bot init

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,38 @@
-package bot
+package main
+
+import (
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/application"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
+
+	"log"
+	"os"
+)
+
+func main() {
+	token := os.Getenv("APP_TELEGRAM_TOKEN")
+	if token == "" {
+		log.Panic("APP_TELEGRAM_TOKEN is not set")
+	}
+
+	cmds := []domain.Command{
+		{
+			Name:        "start",
+			Description: "What this bot can do",
+			Do:          domain.HandleStart,
+		},
+		{
+			Name:        "help",
+			Description: "List all available commands",
+			Do:          domain.HandleHelp,
+		},
+	}
+
+	router := application.NewRouter(cmds)
+
+	bot, err := application.NewBot(token, router)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Start()
+}
