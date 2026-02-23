5babf0344b6c5add310d01d3635f6764410881e5
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-02-23 15:45:30 +0700

    move handlers to application layer

---
+++ internal/application/commands.go
@@ -0,0 +1,28 @@
+package application
+
+import "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
+
+func GetCommands() []domain.Command {
+	return []domain.Command{
+		{
+			Name:        "start",
+			Description: "What this bot can do",
+			Do:          HandleStart,
+		},
+		{
+			Name:        "help",
+			Description: "List all available commands",
+			Do:          HandleHelp,
+		},
+	}
+}
+
+func HandleStart(user domain.User) string {
+	return "Hi! This bot can track updates on your links, so you won't miss on news! /help for list my commands"
+}
+
+func HandleHelp(user domain.User) string {
+	return `Available commands:
+/start - what this bot can do
+/help - list all available commands`
+}
