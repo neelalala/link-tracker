63518cb4b53f249a69e309d6134f5580a2db8146
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-02-23 15:37:46 +0700

    router for commands

---
+++ internal/application/router.go
@@ -0,0 +1,27 @@
+package application
+
+import (
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
+)
+
+type Router struct {
+	commands map[string]domain.Command
+}
+
+func NewRouter(cmds []domain.Command) *Router {
+	r := &Router{
+		commands: make(map[string]domain.Command),
+	}
+	for _, cmd := range cmds {
+		r.commands["/"+cmd.Name] = cmd
+	}
+	return r
+}
+
+func (r *Router) Handle(command string, user domain.User) string {
+	cmd, exists := r.commands[command]
+	if !exists {
+		return "Unknown command. Use /help to list all commands."
+	}
+	return cmd.Do(user)
+}
