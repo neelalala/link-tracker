e20c26fe3a5c6ca86a5716c2db3daae7e88caa03
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-02-23 15:36:20 +0700

    add command handlers

--- internal/domain/command.go
+++ internal/domain/command.go
@@ -5,3 +5,13 @@ type Command struct {
 	Description string
 	Do          func(user User) string
 }
+
+func HandleStart(user User) string {
+	return "Hi! This bot can track updates on your links, so you won't miss on news! /help for list my commands"
+}
+
+func HandleHelp(user User) string {
+	return `Available commands:
+/start - what this bot can do
+/help - list all available commands`
+}
