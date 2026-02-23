1d185597d6b84d002337e2efe58732f35078ccc6
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-02-23 15:46:57 +0700

    move creating commands slice from main to application

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/application"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
-
 	"log"
 	"os"
 )
@@ -14,18 +12,7 @@ func main() {
 		log.Panic("APP_TELEGRAM_TOKEN is not set")
 	}
 
-	cmds := []domain.Command{
-		{
-			Name:        "start",
-			Description: "What this bot can do",
-			Do:          domain.HandleStart,
-		},
-		{
-			Name:        "help",
-			Description: "List all available commands",
-			Do:          domain.HandleHelp,
-		},
-	}
+	cmds := application.GetCommands()
 
 	router := application.NewRouter(cmds)
 
