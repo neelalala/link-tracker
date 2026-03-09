8a2b2044ff803367a566b50d5ca45a6fad051421
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-03-09 23:25:37 +0700

    update notifier with bot api

---
+++ internal/scrapper/infrastructure/adapter/out/notifier/telegram.go
@@ -0,0 +1,68 @@
+package notifier
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/scrapper/domain"
+	"net/http"
+	"time"
+)
+
+const (
+	endpoint = "updates"
+	timeout  = 10 * time.Second
+)
+
+type Bot struct {
+	url        string
+	httpClient *http.Client
+}
+
+func NewBot(url string) *Bot {
+	return &Bot{
+		url:        url,
+		httpClient: &http.Client{Timeout: timeout},
+	}
+}
+
+func (b *Bot) SendUpdate(update domain.LinkUpdate) error {
+	type request struct {
+		Id          int64   `json:"id"`
+		Url         string  `json:"url"`
+		Description string  `json:"description"`
+		TgChatIds   []int64 `json:"tgChatIds"`
+	}
+
+	var reqData = request{
+		Id:          update.ID,
+		Url:         update.URL,
+		Description: update.Description,
+		TgChatIds:   update.TgChatIDs,
+	}
+
+	body, err := json.Marshal(reqData)
+	if err != nil {
+		return fmt.Errorf("failed to marshal update request: %w", err)
+	}
+
+	reqUrl := fmt.Sprintf("%s/%s", b.url, endpoint)
+	req, err := http.NewRequest(http.MethodPost, reqUrl, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := b.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request to bot: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bot api returned unexpected status: %d", resp.StatusCode)
+	}
+
+	return nil
+}
