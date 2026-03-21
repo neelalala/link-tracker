424248b1b910b2ea872287401d254158dcb79fcb
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-03-21 20:12:48 +0700

    add log level to config

--- internal/scrapper/infrastructure/config/config.go
+++ internal/scrapper/infrastructure/config/config.go
@@ -25,6 +25,7 @@ type Config struct {
 	UpdateIntervalSeconds int      `yaml:"update-interval-seconds"`
 	Environment           string   `config:"environment,local"`
 	LogsFile              string   `config:"logs-file,"`
+	LogLevel              string   `config:"log-level,error"`
 	BotUrl                string   `config:"bot-url"`
 	ScrapperApiPort       uint16   `config:"scrapper-api-port"`
 	ApiProtocol           Protocol `config:"api-protocol"`
