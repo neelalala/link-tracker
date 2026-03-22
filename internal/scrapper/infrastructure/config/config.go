b512f0a2dd9067e8c8758a0932e86e04b39ddeb8
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-03-22 19:18:13 +0700

    access type

--- internal/scrapper/infrastructure/config/config.go
+++ internal/scrapper/infrastructure/config/config.go
@@ -6,10 +6,14 @@ import (
 )
 
 type Protocol string
+type AccessType string
 
 const (
 	HTTP Protocol = "http"
 	GRPC Protocol = "grpc"
+
+	SQL     AccessType = "SQL"
+	Builder AccessType = "BUILDER"
 )
 
 func (protocol Protocol) Validate() error {
@@ -21,16 +25,26 @@ func (protocol Protocol) Validate() error {
 	}
 }
 
+func (accessType AccessType) Validate() error {
+	switch accessType {
+	case Builder, SQL:
+		return nil
+	default:
+		return fmt.Errorf("invalid access type: %q. Allowed values are 'SQL' or 'BUILDER'", accessType)
+	}
+}
+
 type Config struct {
-	UpdatesIntervalSeconds int      `yaml:"updates-interval-seconds"`
-	Environment            string   `config:"environment,local"`
-	LogsFile               string   `config:"logs-file,"`
-	LogLevel               string   `config:"log-level,error"`
-	BotUrl                 string   `config:"bot-url"`
-	ScrapperApiPort        uint16   `config:"scrapper-api-port"`
-	ApiProtocol            Protocol `config:"api-protocol"`
-	MigrationsDir          string   `config:"migrations-dir"`
-	DatabaseUrl            string   `config:"database-url"`
+	UpdatesIntervalSeconds int        `config:"updates-interval-seconds"`
+	Environment            string     `config:"environment,local"`
+	LogsFile               string     `config:"logs-file,"`
+	LogLevel               string     `config:"log-level,error"`
+	BotUrl                 string     `config:"bot-url"`
+	ScrapperApiPort        uint16     `config:"scrapper-api-port"`
+	ApiProtocol            Protocol   `config:"api-protocol"`
+	MigrationsDir          string     `config:"migrations-dir"`
+	DatabaseUrl            string     `config:"database-url"`
+	AccessType             AccessType `config:"access-type,builder"`
 }
 
 func Load(configPath string) (*Config, error) {
