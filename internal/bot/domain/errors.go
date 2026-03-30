3eb8c6663b61d75e8ed0cf42b9d279d62045341e
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-03-30 23:21:10 +0700

    error chat not registered or link not found for delete tracked link scrapper method

--- internal/bot/domain/errors.go
+++ internal/bot/domain/errors.go
@@ -3,10 +3,10 @@ package domain
 import "errors"
 
 var (
-	ErrAlreadySubscribed     = errors.New("already subscribed")
-	ErrNotSubscribed         = errors.New("not subscribed")
-	ErrChatAlreadyRegistered = errors.New("chat already registered")
-	ErrChatNotRegistered     = errors.New("chat not registered")
-	ErrUrlNotSupported       = errors.New("url not supported")
-	ErrLinkNotFound          = errors.New("link not found")
+	ErrAlreadySubscribed               = errors.New("already subscribed")
+	ErrNotSubscribed                   = errors.New("not subscribed")
+	ErrChatAlreadyRegistered           = errors.New("chat already registered")
+	ErrChatNotRegistered               = errors.New("chat not registered")
+	ErrUrlNotSupported                 = errors.New("url not supported")
+	ErrChatNotRegisteredOrLinkNotFound = errors.New("chat not registered or link not found")
 )
