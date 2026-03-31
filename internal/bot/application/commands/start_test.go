54a6e422edc9512a0c2d60f58a5bfe48d65b1883
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-03-31 17:03:43 +0700

    tests

---
+++ internal/bot/application/commands/start_test.go
@@ -0,0 +1,72 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/domain"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/mocks"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+func TestStartCommand_Execute(t *testing.T) {
+	type mockBehavior func(s *mocks.MockScrapper, chatID int64)
+
+	tests := []struct {
+		name           string
+		msg            domain.Message
+		mockBehavior   mockBehavior
+		expectedResult string
+		expectedError  bool
+	}{
+		{
+			name: "Success (New User)",
+			msg:  domain.Message{ChatID: 123},
+			mockBehavior: func(s *mocks.MockScrapper, chatID int64) {
+				s.EXPECT().RegisterChat(gomock.Any(), chatID).Return(nil)
+			},
+			expectedResult: startCommandMessageNewUser,
+			expectedError:  false,
+		},
+		{
+			name: "Success (Old User)",
+			msg:  domain.Message{ChatID: 123},
+			mockBehavior: func(s *mocks.MockScrapper, chatID int64) {
+				s.EXPECT().RegisterChat(gomock.Any(), chatID).Return(domain.ErrChatAlreadyRegistered)
+			},
+			expectedResult: startCommandMessageOldUser,
+			expectedError:  false,
+		},
+		{
+			name: "Unexpected Error",
+			msg:  domain.Message{ChatID: 123},
+			mockBehavior: func(s *mocks.MockScrapper, chatID int64) {
+				s.EXPECT().RegisterChat(gomock.Any(), chatID).Return(errors.New("db down"))
+			},
+			expectedResult: startCommandUnexpectedError,
+			expectedError:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			scrapperMock := mocks.NewMockScrapper(ctrl)
+			tt.mockBehavior(scrapperMock, tt.msg.ChatID)
+
+			cmd := NewStartCommand(scrapperMock, discardLogger())
+
+			res, err := cmd.Execute(context.Background(), tt.msg)
+
+			assert.Equal(t, tt.expectedResult, res)
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
---
+++ internal/bot/application/commands/untrack_test.go
@@ -0,0 +1,68 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/domain"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/mocks"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+func TestUntrackCommand_Execute(t *testing.T) {
+	type mockBehavior func(t *testing.T, r *mocks.MockSessionRepository, chatID int64)
+
+	tests := []struct {
+		name           string
+		msg            domain.Message
+		mockBehavior   mockBehavior
+		expectedResult string
+		expectedError  bool
+	}{
+		{
+			name: "Success",
+			msg:  domain.Message{ChatID: 123},
+			mockBehavior: func(t *testing.T, r *mocks.MockSessionRepository, chatID int64) {
+				session := domain.NewSession(chatID)
+				r.EXPECT().GetOrCreate(gomock.Any(), chatID).Return(session, nil)
+
+				expectedSession := session
+				expectedSession.State = domain.StateWaitingForURLUntrack
+				r.EXPECT().Save(gomock.Any(), expectedSession).Return(nil)
+			},
+			expectedResult: untrackCommandTrackingSuccessfully,
+			expectedError:  false,
+		},
+		{
+			name: "Error getting session",
+			msg:  domain.Message{ChatID: 123},
+			mockBehavior: func(t *testing.T, r *mocks.MockSessionRepository, chatID int64) {
+				r.EXPECT().GetOrCreate(gomock.Any(), chatID).Return(domain.Session{}, errors.New("unexpected error"))
+			},
+			expectedResult: untrackCommandUnexpectedError,
+			expectedError:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repoMock := mocks.NewMockSessionRepository(ctrl)
+			tt.mockBehavior(t, repoMock, tt.msg.ChatID)
+
+			cmd := NewUntrackCommand(repoMock, discardLogger())
+
+			res, err := cmd.Execute(context.Background(), tt.msg)
+
+			assert.Equal(t, tt.expectedResult, res)
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
