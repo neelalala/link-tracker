b24a6fbaf30aba324964add9982ff49abd623ac0
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-05-11 14:41:03 +0300

    add outbox to domain

---
+++ internal/scrapper/domain/outbox.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"context"
+	"time"
+)
+
+type OutboxStatus string
+
+const (
+	OutboxStatusPending OutboxStatus = "PENDING"
+	OutboxStatusSent    OutboxStatus = "SENT"
+	OutboxStatusFailed  OutboxStatus = "FAILED"
+)
+
+type Outbox struct {
+	ID        int64
+	Topic     string
+	Payload   []byte
+	Status    OutboxStatus
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type OutboxRepository interface {
+	Add(ctx context.Context, topic string, payload []byte) (Outbox, error)
+	GetPending(ctx context.Context, limit int) ([]Outbox, error)
+	GetFailed(ctx context.Context, limit int) ([]Outbox, error)
+	UpdateStatus(ctx context.Context, id int64, status OutboxStatus) error
+}
