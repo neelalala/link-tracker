f27b23f4196f53a2b2457e6b70cc5875de6bf7a7
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-05-21 18:39:01 +0700

    link validation

--- internal/bot/domain/link.go
+++ internal/bot/domain/link.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/validation"
 )
 
 type TrackedLink struct {
@@ -20,4 +21,27 @@ type LinkUpdate struct {
 
 type LinkUpdateHandler interface {
 	HandleUpdate(ctx context.Context, update LinkUpdate) error
+}
+
+func (update LinkUpdate) Validate() validation.Problems {
+	problems := make(validation.Problems)
+
+	if update.ID <= 0 {
+		problems.Add("id", "must be positive")
+	}
+
+	if update.URL == "" {
+		problems.Add("url", "must not be empty")
+	}
+
+	if update.Description == "" && update.Preview == "" {
+		problems.Add("description", "either description or preview must be set")
+		problems.Add("preview", "either description or preview must be set")
+	}
+
+	if len(update.TgChatIDs) == 0 {
+		problems.Add("tgChatIDs", "must contain least one chat id")
+	}
+
+	return problems
 }
