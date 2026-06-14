6243412997ac0341ae4a2e0595fb98b8f67b3a45
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-06-14 17:00:01 +0700

    kafka inbound adapter

---
+++ internal/agent/infrastructure/adapter/in/kafka/listener.go
@@ -0,0 +1,126 @@
+package kafka
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/IBM/sarama"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/agent/domain"
+)
+
+type UpdateHandler interface {
+	HandleUpdate(ctx context.Context, update domain.LinkUpdate) error
+}
+
+type Listener struct {
+	consumer sarama.ConsumerGroup
+	producer sarama.SyncProducer
+	handler  *Handler
+	topic    string
+
+	ctx    context.Context
+	cancel context.CancelFunc
+	done   chan struct{}
+	log    *slog.Logger
+}
+
+func NewListener(
+	brokers []string,
+	consumerGroup string,
+	topic string,
+	dlqTopic string,
+	delay time.Duration,
+	maxDelay time.Duration,
+	backoffFactor float64,
+	retries int,
+	registryURL string,
+	updateHandler UpdateHandler,
+	log *slog.Logger,
+) (*Listener, error) {
+	config := newConfig()
+
+	consumer, err := sarama.NewConsumerGroup(brokers, consumerGroup, config)
+	if err != nil {
+		return nil, err
+	}
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		consumer.Close()
+		return nil, fmt.Errorf("failed to create producer for dlq: %w", err)
+	}
+
+	handler := NewHandler(
+		updateHandler,
+		producer,
+		dlqTopic,
+		delay,
+		maxDelay,
+		backoffFactor,
+		retries,
+		registryURL,
+		log,
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &Listener{
+		consumer: consumer,
+		producer: producer,
+		handler:  handler,
+		topic:    topic,
+		ctx:      ctx,
+		cancel:   cancel,
+		done:     make(chan struct{}),
+		log:      log,
+	}, nil
+}
+
+func (listener *Listener) Start() error {
+	defer close(listener.done)
+
+	for {
+		if err := listener.consumer.Consume(listener.ctx, []string{listener.topic}, listener.handler); err != nil {
+			return err
+		}
+
+		if listener.ctx.Err() != nil {
+			return nil
+		}
+	}
+}
+
+func (listener *Listener) Stop(ctx context.Context) error {
+	listener.log.Info("Shutting down kafka listener...")
+
+	if listener.cancel != nil {
+		listener.cancel()
+	}
+
+	select {
+	case <-listener.done:
+	case <-ctx.Done():
+		listener.log.Warn("Kafka listener shutdown context exceeded")
+	}
+
+	errConsumer := listener.consumer.Close()
+
+	errProducer := listener.producer.Close()
+
+	err := errors.Join(errConsumer, errProducer)
+
+	return err
+}
+
+func (listener *Listener) WaitReady(ctx context.Context) error {
+	select {
+	case <-listener.handler.ready:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
---
+++ internal/agent/infrastructure/adapter/in/kafka/mapper/link_update.go
@@ -0,0 +1,64 @@
+package mapper
+
+import (
+	"fmt"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/agent/domain"
+)
+
+func LinkUpdateFromNative(native any) (domain.LinkUpdate, error) {
+	record, ok := native.(map[string]any)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to map[string]any")
+
+	}
+
+	id, ok := record["id"].(int64)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to int64 id")
+	}
+
+	url, ok := record["url"].(string)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to string url")
+	}
+
+	author, ok := record["author"].(string)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to string author")
+	}
+
+	description, ok := record["description"].(string)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to string description")
+	}
+
+	preview, ok := record["preview"].(string)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast native to string preview")
+	}
+
+	update := domain.LinkUpdate{
+		ID:          id,
+		URL:         url,
+		Author:      author,
+		Description: description,
+		Preview:     preview,
+	}
+
+	ids, ok := record["tgChatIds"].([]any)
+	if !ok {
+		return domain.LinkUpdate{}, fmt.Errorf("failed to cast tgChatIds to []any")
+	}
+
+	update.TgChatIDs = make([]int64, len(ids))
+	for i, val := range ids {
+		id, ok := val.(int64)
+		if !ok {
+			return domain.LinkUpdate{}, fmt.Errorf("failed to cast tgChatIds[%d] to int64", i)
+		}
+		update.TgChatIDs[i] = id
+	}
+
+	return update, nil
+}
